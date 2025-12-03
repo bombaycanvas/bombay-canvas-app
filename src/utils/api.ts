db9ffe25f08ec087a19d86aaabb7c768cdb913ce
jaril5976 <rajjariwala5976@gmail.com>
2025-12-03 02:07:54 -0800

    fix: google login issue

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,6 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { NEXT_PUBLIC_BASE_URL } from '@env';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useAuthStore } from '../store/authStore';
-console.log('NEXT_PUBLIC_BASE_URL', NEXT_PUBLIC_BASE_URL);
 
 export const getToken = async (key: string): Promise<string | null> => {
   try {
